f307ee9be171e6096ab76863c426415445b43de6
Jonathan Lipps <jlipps@gmail.com>
2012-03-01 18:20:01 -0800

    basic html styling + javascript window resizing

--- js/site.js
+++ js/site.js
@@ -3,5 +3,18 @@ var Album = window.Album || {};
 
 // on document ready
 $(function() {
-
+    Album.resizeMainContent();
+    Album.trackWindowResize();
 });
+
+Album.resizeMainContent = function() {
+    var headerAndFooter = $('header').height() + $('footer').height();
+    // make height of #main fill the rest of the page
+    $('#main').css({
+        height: ($(window).height() - headerAndFooter) + 'px'
+    });
+};
+
+Album.trackWindowResize = function() {
+    $(window).resize(Album.resizeMainContent);
+}
